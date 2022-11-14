11506df36ea393c18897d7385a4a56535042f1cb
Daniel Weper Jensen <danielweper@gmail.com>
2022-11-14 13:14:18 +0100

    Start of Game class

---
+++ src/main/java/gruppe27/Game.java
@@ -0,0 +1,10 @@
+package gruppe27;
+
+import java.util.Queue;
+
+public class Game {
+    private Player[] players;
+    private Board board;
+    private Queue<ChanceCard> cards;
+    // private DieCup cup;
+}
