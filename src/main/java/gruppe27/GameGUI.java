9954b265af4290d9bce9b74c3aafd16210e9f26c
ZEE <Zaidsh02@gmail.com>
2022-11-15 00:45:37 +0100

    GUI Board almost done

--- src/main/java/gruppe27/GameGUI.java
+++ src/main/java/gruppe27/GameGUI.java
@@ -1,4 +1,22 @@
 package gruppe27;
+import javax.swing.*;
 
-public class GameGUI {
+public class GameGUI extends JFrame{
+    private JPanel Board;
+    private JPanel BoardMid;
+    private JPanel Start;
+
+    public GameGUI(String title){
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(Board);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        JFrame game = new GameGUI("Try 1");
+        game.setVisible(true);
+    }
+
 }
