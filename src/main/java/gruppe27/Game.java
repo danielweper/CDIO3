d5402b5ecbc236737d5dccf41332e5d2f348e471
Daniel <danielweper@gmail.com>
2022-11-14 16:12:31 +0100

    Made the move to color chance cards

--- src/main/java/gruppe27/Game.java
+++ src/main/java/gruppe27/Game.java
@@ -29,8 +29,9 @@ public class Game {
 
     private List<ChanceCard> generateChanceCards() {
         List<ChanceCard> allChanceCards = new ArrayList<>();
-        // Commonly used
+        // Commonly used actions
         ChanceCardAction takeAnotherCard = new ChanceCardAction(ChanceCardEvent.TAKE_ANOTHER_CARD, 1);
+        ChanceCardAction getForFreeOrPayOwner = new ChanceCardAction(ChanceCardEvent.GET_PROPERTY_FREE_OR_PAY_OWNER, 0);
 
         // Make all the give to other players cards
         for (int i = 0; i < 4; i++) {
@@ -42,7 +43,7 @@ public class Game {
         allChanceCards.add(ChanceCard.SingleActionCard(new ChanceCardAction(ChanceCardEvent.MOVE_TO, 23)));
 
         // Make the move to and pay card
-        allChanceCards.add(ChanceCard.MultiActionCard(new ChanceCardAction(ChanceCardEvent.MOVE_TO, 10), new ChanceCardAction(ChanceCardEvent.GET_PROPERTY_FREE_OR_PAY_OWNER, 0)));
+        allChanceCards.add(ChanceCard.MultiActionCard(new ChanceCardAction(ChanceCardEvent.MOVE_TO, 10), getForFreeOrPayOwner.copy()));
 
         // Make the pay and get paid cards
         allChanceCards.add(ChanceCard.SingleActionCard(new ChanceCardAction(ChanceCardEvent.PAY_BANK, 2)));
@@ -63,7 +64,21 @@ public class Game {
         allChanceCards.add(ChanceCard.SingleActionCard(new ChanceCardAction(ChanceCardEvent.GET_OUT_OF_JAIL_FREE, 0)));
 
         // Make all the move to specific color cards
-        // TODO
+        Color[] specificColors = new Color[] {Color.ORANGE, Color.LIGHT_BLUE, Color.RED};
+        for (int i = 0; i < 3; i++) {
+            ChanceCardAction moveToColorAction = new ChanceCardAction(ChanceCardEvent.MOVE_TO_COLOR, specificColors[i].ordinal());
+            allChanceCards.add(ChanceCard.MultiActionCard(moveToColorAction, getForFreeOrPayOwner.copy()));
+        }
+
+        // Make all the move to choice of cards
+        Color[][] colorChoices = new Color[][] { {Color.ORANGE, Color.GREEN}, {Color.PINK, Color.DARK_BLUE}, {Color.LIGHT_BLUE, Color.RED}, {Color.BROWN, Color.YELLOW}};
+        for (int i = 0; i < 4; i++) {
+            Color[] choices = colorChoices[i];
+            ChanceCardAction moveToFirstColorAction = new ChanceCardAction(ChanceCardEvent.MOVE_TO_COLOR, choices[0].ordinal());
+            ChanceCardAction moveToSecondColorAction = new ChanceCardAction(ChanceCardEvent.MOVE_TO_COLOR, choices[1].ordinal());
+            allChanceCards.add(ChanceCard.MultiChoiceCard(moveToFirstColorAction, moveToSecondColorAction));
+            // TODO find en bedre løsning?
+        }
 
         // Return the made array
         return allChanceCards;
