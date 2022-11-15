816c2e1511e1e404c497440236607d1a01bfa254
Daniel Weper Jensen <danielweper@gmail.com>
2022-11-15 15:55:54 +0100

    Fixed player

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,8 +1,10 @@
 public class Player {
     public final String Name;
     public final Account Account;
-    public Player(String playerName, int StartingBalance, int PlayerID){
+    public final int ID;
+    public Player(String playerName, int StartingBalance, int playerId){
         this.Name = playerName;
         this.Account = new Account(StartingBalance);
+        this.ID = playerId;
     }
 }
