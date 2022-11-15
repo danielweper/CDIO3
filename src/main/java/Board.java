430f8d40f2512183deb5844460d2d88f04b9d4a7
Zigalow <s224302@dtu.dk>
2022-11-15 15:36:38 +0100

    Board-klasse er færdigt. Sådan helt færdig..

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,71 +1,40 @@
-import java.util.Random;
+package gruppe27;
 
 public class Board {
 
 
-    // Er den der skal bruges, når dice er blevet oprettet.
-    int diceFaceValue;
+    private int numberOfFields;
+    private GameField[] fields;
+    private int[] playerIndexesOnBoard;
 
-    // Har oprettet til vist det bliver nødvendigt at starte et specifikt sted fra
-    int startField = 0;
-
-    int numberOfFields;
-
-    // Tænkte til felterne, men kan godt være at logikken for felterne ikke har nogen function i denne klasse
-    int[] listOfFields;
-    int presentField;
-
-    // Til test
-    int randomDiceFaceValue;
-
-    Board(int numberOfFields) {
-        this.numberOfFields = numberOfFields;
-
-        this.listOfFields = new int[numberOfFields];
-
-    }
+    Board(GameField[] fields, int numberOfPlayers, int startField) {
+        this.fields = fields;
+        this.numberOfFields = fields.length;
+        this.playerIndexesOnBoard = new int[numberOfPlayers];
 
 
-    // Til test
-    Random rng = new Random();
-
-
-    public int roll() {
-
-        // Brugt random til test. Bruger bare den rigtige dice klasse når den er oprettet
-
-        this.randomDiceFaceValue = rng.nextInt(6) + 1;
-
-        return this.randomDiceFaceValue;
-    }
-
-    // Afgør hvor man lander.
-
-    public int getPresentField() {
-        this.presentField = this.randomDiceFaceValue + this.presentField;
-
-        // Når man fx. slår 5 og står på felt nr. 9, i et spil med 12 felter.
-        // Dette gør at man bliver indenfor felterne
-        if (numberOfFields <= this.presentField) {
-            this.presentField = this.presentField - numberOfFields;
+        for (int i = 0; i < numberOfPlayers; i++) {
+            this.playerIndexesOnBoard[i] = startField;
         }
-
-        return this.presentField;
     }
 
-    public static void main(String[] args) {
 
-        // Til testning for at se om det virker:
+    public GameField movePlayerByAmount(int playerByIndex, int moveAmount) {
+        int currentPlayerIndexOnBoard = this.playerIndexesOnBoard[playerByIndex];
+        int newIndexOnBoard = currentPlayerIndexOnBoard + moveAmount;
 
-        Board k1 = new Board(12);
 
-        for (int i = 0; i <= 5; i++) {
-            System.out.println("The present field is " + k1.presentField);
-            System.out.println("The face value is " + k1.roll());
-            System.out.println("Landed on index " + k1.getPresentField() + "\n");
-        }
+        newIndexOnBoard = newIndexOnBoard % numberOfFields;
 
+        return fields[newIndexOnBoard];
+
+    }
+
+    public GameField movePlayerToField(int playerByIndex, int field) {
+        this.playerIndexesOnBoard[playerByIndex] = field;
+        return fields[field];
     }
 
 
 }
+
