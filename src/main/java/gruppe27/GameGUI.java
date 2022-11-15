8e85ad080c1170f048d973b8a78c2712f92f9a86
ZEE <Zaidsh02@gmail.com>
2022-11-15 13:33:13 +0100

    GUI showing

--- src/main/java/gruppe27/GameGUI.java
+++ src/main/java/gruppe27/GameGUI.java
@@ -5,17 +5,19 @@ public class GameGUI extends JFrame{
     private JPanel Board;
     private JPanel BoardMid;
     private JPanel Start;
+    private JPanel Field1;
 
     public GameGUI(String title){
         super(title);
-
+        this.setSize(1000,1000);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(Board);
         this.pack();
     }
 
     public static void main(String[] args) {
-        JFrame game = new GameGUI("Try 1");
+        JFrame game = new JFrame("Try 1");
+        game.setContentPane(new GameGUI("GAME").Board);
         game.setVisible(true);
     }
 
