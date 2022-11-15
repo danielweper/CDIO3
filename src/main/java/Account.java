b4a240093fe7303347cc641aca879997f9f58515
Daniel Weper Jensen <danielweper@gmail.com>
2022-11-15 15:53:30 +0100

    Clean up in Account class

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,28 +1,22 @@
-package gruppe27;
-
-import javax.swing.*;
-
 public class Account {
-
     private int balance;
-
     public Account(int startBalance){
         this.balance = startBalance;
     }
 
-    // Getter
     public int getBalance() {
         return balance;
     }
 
-    //Setter
-    public void getPaid(int effectValue) {this.balance = this.balance + (effectValue);}
-    public void loseMoney(int effectValue) {this.balance = this.balance - (effectValue);}
+    public void getPaid(int effectValue) {
+        this.balance = this.balance + (effectValue);
+    }
+
+    public void loseMoney(int effectValue) {
+        this.balance = this.balance - (effectValue);
+    }
 
     public boolean isBankrupt() {
-        if (balance < 0){
-            return true;
-    }
-        else {return false;}
+        return balance < 0;
     }
 }
