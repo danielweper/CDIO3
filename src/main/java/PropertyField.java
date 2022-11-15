f075f08e5a96a632ae9946089adf63ce9abd5fa3
Daniel Weper Jensen <danielweper@gmail.com>
2022-11-15 16:21:30 +0100

    Clean up in Property Field

--- src/main/java/PropertyField.java
+++ src/main/java/PropertyField.java
@@ -1,25 +1,29 @@
 public class PropertyField extends GameField{
-    int fieldPrice;
-    Color color;
-    Player owner;
-    public PropertyField(int fieldPris,Color color){
-        this.fieldPrice =fieldPris;
-        this.color=color;
-        this.owner=null;
+    public final int Value;
+    public final Color Color;
+    private Player owner;
+    public PropertyField(int fieldPris, Color color){
+        this.Value = fieldPris;
+        this.Color = color;
+        this.owner = null;
     }
-    public int  assessment(){
-        return fieldPrice;
+
+    public void setOwner(Player newOwner) {
+        this.owner = newOwner;
     }
-    public Color fieldColor(){
-        return color;
+
+    public Player getOwner() {
+        return owner;
     }
 
     @Override
     public LandOnAction landedOn(Player player) {
-        if(owner==null)
-        return LandOnAction.BUY_PROPERTY;
-        if(owner.equals(player))
+        if (owner == null) {
+            return LandOnAction.BUY_PROPERTY;
+        }
+        if (owner.equals(player)) {
             return LandOnAction.NOTHING;
+        }
         return LandOnAction.PAY_RENT;
     }
 
