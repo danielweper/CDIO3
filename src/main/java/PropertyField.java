c6b4baf82d64b6d2e3478bbd20f3ba5bf6c19c6d
Felix Schmidt <Felix.Blicher.Schmidt@gmail.com>
2022-11-15 16:18:26 +0100

    PropertyField BUY_PROPERTY - PAY_RENT - VISITING

--- src/main/java/PropertyField.java
+++ src/main/java/PropertyField.java
@@ -1,15 +1,26 @@
 public class PropertyField extends GameField{
     int fieldPrice;
     Color color;
+    Player owner;
     public PropertyField(int fieldPris,Color color){
         this.fieldPrice =fieldPris;
         this.color=color;
+        this.owner=null;
     }
     public int  assessment(){
         return fieldPrice;
     }
-    public Color type(){
+    public Color fieldColor(){
         return color;
+    }
+
+    @Override
+    public LandOnAction landedOn(Player player) {
+        if(owner==null)
+        return LandOnAction.BUY_PROPERTY;
+        if(owner.equals(player))
+            return LandOnAction.NOTHING;
+        return LandOnAction.PAY_RENT;
     }
 
 }
