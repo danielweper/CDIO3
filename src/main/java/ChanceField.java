846e2655b077dbf78ae9e7d43d3f671cea4d9e57
Felix Schmidt <Felix.Blicher.Schmidt@gmail.com>
2022-11-15 15:31:34 +0100

    ChanceField returns DRAW_CHANCE_CARD

--- src/main/java/ChanceField.java
+++ src/main/java/ChanceField.java
@@ -1,3 +1,6 @@
-public class ChanceField {
-
+public class ChanceField extends GameField {
+    @Override
+    public LandOnAction landedOn(Player player) {
+        return LandOnAction.DRAW_CHANCE_CARD;
+    }
 }
