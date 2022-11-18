046e2bb7428cf58c16de509c6b57cc7f0c8b4066
ZEE <Zaidsh02@gmail.com>
2022-11-18 11:12:29 +0100

    PlayerGUI and FieldGUI started

---
+++ src/main/java/FieldGUI.java
@@ -0,0 +1,15 @@
+import gui_fields.GUI_Field;
+import gui_main.GUI;
+
+import java.awt.Color;
+
+public class FieldGUI {
+
+    public static void Fieldgenerator(){
+        GUI_Field[] fields = new GUI_Field[24];
+
+        GUI gui = new GUI(fields, Color.RED);
+
+
+    }
+}
--- src/main/java/PlayerGUI.java
+++ src/main/java/PlayerGUI.java
@@ -1,6 +1,11 @@
 import gui_fields.GUI_Car;
 import gui_fields.GUI_Player;
 
-public class PlayerGUI {
+public class PlayerGUI extends GUI_Player{
+
+    public PlayerGUI(String name){
+        super(name);
+    }
+
 
 }
