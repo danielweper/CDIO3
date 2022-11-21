503b76e2b6fa99ff287973cd6768948a2253fbc1
ZEE <Zaidsh02@gmail.com>
2022-11-21 13:04:09 +0100

    PlayerGUI Changes

--- src/main/java/PlayerGUI.java
+++ src/main/java/PlayerGUI.java
@@ -6,6 +6,12 @@ public class PlayerGUI extends GUI_Player{
     public PlayerGUI(String name){
         super(name);
     }
+    public PlayerGUI(String name,int balance){
+        super(name, balance);
+    }
+    public PlayerGUI(String name,int balance, GUI_Car car){
+        super(name, balance, car);
+    }
 
 
 }
