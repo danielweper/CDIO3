16ba6b31e2f4878c6c4e024513dd81424f774952
ZEE <Zaidsh02@gmail.com>
2022-11-22 17:10:34 +0100

    Player pick Name, Color and car

--- src/main/java/BoardGUI.java
+++ src/main/java/BoardGUI.java
@@ -4,6 +4,7 @@ import gui_main.GUI;
 import java.awt.Color;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class BoardGUI{
         static FieldGUI num_Fields = new FieldGUI();
@@ -13,7 +14,7 @@ public class BoardGUI{
                 SixSidedDie d1 = new SixSidedDie();
                 SixSidedDie d2 = new SixSidedDie();
 
-                Player[] players = makePlayers(4, gui);
+                GUI_Player[] players = makePlayers(4, gui);
 
 
                 //https://github.com/diplomit-dtu/MatadorGUIGuide/blob/3.2.x/src/main/java/Terning.java
@@ -27,11 +28,26 @@ public class BoardGUI{
                 }
 
         }
-        public static Player[] makePlayers(int amount, GUI gui) {
+        public static GUI_Player[] makePlayers(int amount, GUI gui) {
                 int startingBalance = (amount == 2) ? 20 : ((amount == 3) ? 18 : 16);
-                Player[] players = new Player[amount];
-                for (int i=0; i < amount; i++){
-                        players[i] = new Player(gui.getUserString("Player " + (i+1) + " what is your name?"), startingBalance, i);
+                ArrayList<Color> colors = new ArrayList<Color>(Arrays.stream(new Color[] {Color.red, Color.blue, Color.black, Color.green, Color.magenta, Color.yellow}).toList());
+                ArrayList<String> colorStrings = new ArrayList<>();
+                for (Color color : colors) {
+                        colorStrings.add(color.toString());
+                }
+
+                GUI_Player[] players = new GUI_Player[amount];
+                for (int i=0; i < amount; i++) {
+                        String playerName = gui.getUserString("Player " + (i+1) + " what is your name?");
+                        String[] colorStringArray = new String[colorStrings.size()];
+                        String colorString = gui.getUserButtonPressed("Choose a color", colorStrings.toArray(colorStringArray));
+                        Color playerColor = colors.get(colorStrings.indexOf(colorString));
+
+                        GUI_Car playerCar = new GUI_Car(playerColor, playerColor, GUI_Car.Type.CAR, GUI_Car.Pattern.FILL);
+                        players[i] = new GUI_Player(playerName, startingBalance, playerCar);
+
+                        colors.remove(playerColor);
+                        colorStrings.remove(colorString);
                 }
                 return players;
         }
