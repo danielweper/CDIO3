15ba3115facc5e0f920271e27b7aeab4bae2087e
Daniel Weper Jensen <danielweper@gmail.com>
2022-11-22 13:48:12 +0100

    Added the ending field to the player movement class

--- src/main/java/PlayerMovement.java
+++ src/main/java/PlayerMovement.java
@@ -2,10 +2,12 @@ public class PlayerMovement {
     public final int StartIndex;
     public final int EndIndex;
     public final boolean PassedStart;
+    public final GameField EndField;
 
-    public PlayerMovement(int startIndex, int endIndex) {
+    public PlayerMovement(int startIndex, int endIndex, GameField endField) {
         this.StartIndex = startIndex;
         this.EndIndex = endIndex;
         this.PassedStart = endIndex < startIndex;
+        this.EndField = endField;
     }
 }
