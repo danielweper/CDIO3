38fd8a8f5f89e19128caf8fe589cb567f88454f1
Daniel Weper Jensen <danielweper@gmail.com>
2022-11-25 10:45:57 +0100

    Fixed board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -17,8 +17,44 @@ public class Board {
     }
 
     public static GameField[] generateStandardFields() {
-        GameField[] GameField = new GameField[0];
-        return GameField;
+        // Generate all the fields in the board
+        GameField[] fields = new GameField[24];
+
+        PropertyColor[] propertyPropertyColors = new PropertyColor[] {PropertyColor.BROWN, PropertyColor.LIGHT_BLUE, PropertyColor.MAGENTA, PropertyColor.ORANGE, PropertyColor.RED, PropertyColor.YELLOW};
+
+        // Make the first 3 sides of the board
+        int fieldIndex = 0;
+        for (int i = 0; i < 3; ++i) {
+            // Generate the "rest" field (start, visiting & free parking)
+            fields[fieldIndex++] = new GameField();
+            int propertyPrice = i+1;
+            int colorIndex = i * 2;
+
+            // Generate the first pair of properties (brown, magenta & red)
+            fields[fieldIndex++] = new PropertyField(propertyPrice, propertyPropertyColors[colorIndex]);
+            fields[fieldIndex++] = new PropertyField(propertyPrice, propertyPropertyColors[colorIndex]);
+
+            // Generate the chance field in the middle of the side
+            fields[fieldIndex++] = new ChanceField();
+
+            ++colorIndex;
+            // Generate the second pair of properties (light blue, orange & yellow)
+            fields[fieldIndex++] = new PropertyField(propertyPrice, propertyPropertyColors[colorIndex]);
+            fields[fieldIndex++] = new PropertyField(propertyPrice, propertyPropertyColors[colorIndex]);
+        }
+
+        // Generate the last side
+        fields[fieldIndex++] = new GoToPrisonField();
+        // Generate the green pair
+        fields[fieldIndex++] = new PropertyField(4, PropertyColor.GREEN);
+        fields[fieldIndex++] = new PropertyField(4, PropertyColor.GREEN);
+        // Generate the chance field on the last side
+        fields[fieldIndex++] = new ChanceField();
+        // Generate the dark blue pair
+        fields[fieldIndex++] = new PropertyField(5, PropertyColor.DARK_BLUE);
+        fields[fieldIndex++] = new PropertyField(5, PropertyColor.DARK_BLUE);
+
+        return fields;
     }
 
 
@@ -40,7 +76,7 @@ public class Board {
     }
 
     public int getPlayerPosition(int playerByIndex) {
-        return 0;
+        return playerIndexesOnBoard[playerByIndex];
     }
 }
 
